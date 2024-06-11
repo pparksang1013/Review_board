187bb3ea7724cba55596fa660a9cc14230f7abb5
pparksang <pparksang1013@gmail.com>
2024-06-12 02:53:43 +0900

    💅STYLE Tailwind css 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ReviewPage } from "./pages/ReviewPage";
+import "./style/index.css";
 
 const router = createBrowserRouter([
     {
